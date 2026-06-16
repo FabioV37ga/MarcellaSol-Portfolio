f5f42e11b8d17425f8edb3dc7ad0973f0e96a7c0
FabioV37ga <veigabfabio@hotmail.com>
2026-06-16 16:31:26 -0300

    teste

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,7 +1,7 @@
 var enviroment = window.location.hostname === 'localhost' ? 'development' : 'production';
 
 export const config = {
-  apiBaseUrl: enviroment === 'development' ? 'http://localhost:3000' : 'https://marcellasol.com.br:3000',
+  apiBaseUrl: enviroment === 'development' ? 'http://localhost:3000' : 'http://marcellasol.com.br:3000',
 };
 
 // Verificar saúde do servidor
