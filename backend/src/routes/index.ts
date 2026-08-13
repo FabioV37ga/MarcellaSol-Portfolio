851bcc5c1b617eb065865309d244a859e7cb68b1
FabioV37ga <veigabfabio@hotmail.com>
2026-08-13 00:56:45 -0300

    ajustes...

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,7 +3,10 @@ import testRoutes from "./testRoutes.js";
 import adminRoutes from "./adminRoutes.js";
 
 const routes = (app:any) => {
-    app.use(express.json(), testRoutes, adminRoutes);
+    app.use(express.json());
+    
+    const prefix = process.env.NODE_ENV === 'production' ? '/api' : '';
+    app.use(prefix, testRoutes, adminRoutes);
 }
 
 export default routes;
