27a23d06189dd3facea8697d6a8dce44e8dbb40e
FabioV37ga <veigabfabio@hotmail.com>
2026-08-12 23:56:16 -0300

    alteração no seletor de collections

--- backend/src/models/projeto.ts
+++ backend/src/models/projeto.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const projetoSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, required: true },
     id: { type: Number, required: true }
-})
+},{collection: 'projetos'}
+)
 
 export default mongoose.model('Projeto', projetoSchema);
